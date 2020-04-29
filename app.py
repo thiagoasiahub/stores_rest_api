62367d68a4540f107127f560aee717aaaddc5e5f
Thiago R.  da Costa <thiagor.dacosta@Mac-mini.local>
2020-04-29 13:28:59 -0300

    Misspelling on requirements.

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
