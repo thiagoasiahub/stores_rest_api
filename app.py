8c9ef7fee7c2218150699b5c0fdcebc3d1056452
Thiago R.  da Costa <thiagor.dacosta@Mac-mini.local>
2020-04-29 13:50:00 -0300

    Changed the place of 'from db import db'

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
@@ -26,5 +26,6 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
