4c8db953188f1ca3100e37d4034044fd6be1c6cf
Thiago R.  da Costa <thiagor.dacosta@Mac-mini.local>
2020-04-29 14:09:14 -0300

    Review resoures/item.py.

--- resources/item.py
+++ resources/item.py
@@ -49,10 +49,10 @@ class Item(Resource):
 
         item = ItemModel.find_by_name(name)
 
-        if item is None:
-            item = ItemModel(name, **data)
-        else:
+        if item:
             item.price = data['price']
+        else:
+            item = ItemModel(name, **data)
 
         item.save_to_db()
 
